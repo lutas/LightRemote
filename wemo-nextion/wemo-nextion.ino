b6fa7a0371f10eb825f004edec08ef956840df64
Phil Lutas <phil@lutas.co.uk>
2018-02-04 19:07:06 +0000

    Initially working

--- wemo-nextion/wemo-nextion.ino
+++ wemo-nextion/wemo-nextion.ino
@@ -14,10 +14,23 @@
 
 ESP8266WiFiMulti WiFiMulti;
 
-Nextion nex(Serial);
+SoftwareSerial nextionSerial(D2, D1);
+
+Nextion nex(nextionSerial);
 NextionDualStateButton btn(nex, 0, 1, "btnOnOff");
 
+const int DELAY_MILLIS = 50;
+const int EXTERNAL_LIGHT_CHECK_SECONDS = 5;
+const int REFRESH_LIGHT_COUNT = EXTERNAL_LIGHT_CHECK_SECONDS * 1000 / DELAY_MILLIS;
+
+#define LIGHT_API PI_URL "light/1"
+
+bool g_prevButtonState = false;
+int g_updateWait = REFRESH_LIGHT_COUNT;
+
 void setLightName(const char* szName) {
+  Serial.printf("Setting light name to %s", szName);
+  
   NextionText text(nex, 0, 2, "t0");
   text.setText((char*)szName);
 }
@@ -25,9 +38,8 @@ void setLightName(const char* szName) {
 void lightBtnPressed(NextionEventType type, INextionTouchable *widget)
 {
   Serial.println("lightBtnPressed");
-  if (btn.isActive())
+  if (type == NEX_EVENT_PUSH)
   {
-    callHTTP(PI_URL "light/0/off");
   }
   else if (type == NEX_EVENT_POP)
   {
@@ -37,28 +49,41 @@ void lightBtnPressed(NextionEventType type, INextionTouchable *widget)
 
 void setup() {    
 
-    WiFiMulti.addAP(WIFI_AP, WIFI_PWD);
-
     Serial.begin(9600);
+    Serial.println("Starting application");
+    
+    nextionSerial.begin(9600);
     nex.init();
+    //btn.attachCallback(&lightBtnPressed);
+    
+    WiFiMulti.addAP(WIFI_AP, WIFI_PWD);
+    bool gotData = false;
+    int attempts = 0;
+    while (!gotData && attempts < 10) {
+      gotData = refreshLightData();
+      if (gotData) {
+        Serial.println("Got light data");
+      } else {
+        Serial.println("Failed to get light data");
+        delay(1000);
+      }
 
-    getLightData(PI_URL "light/0");
-    btn.attachCallback(&lightBtnPressed);
+      ++attempts;
+    }
 }
 
-bool getLightData(const char* szUrl) {
-
-  Serial.println("getting light data");
+bool refreshLightData() {
   if((WiFiMulti.run() == WL_CONNECTED)) {
     HTTPClient http;
-    http.begin(szUrl);
+    http.begin(LIGHT_API);
   
     // start connection and send HTTP header
     int httpCode = http.GET();
 
     // httpCode will be negative on error
     if(httpCode < 0) {
-      Serial.println("error");
+      Serial.println("error getting light data");
+      http.end();
       return false;
     }
      
@@ -69,6 +94,7 @@ bool getLightData(const char* szUrl) {
 
     if (!root.success()) {
       Serial.println("Failed to parse JSON");
+      http.end();
       return false;
     }
 
@@ -76,16 +102,22 @@ bool getLightData(const char* szUrl) {
     setLightName(szLightName);
 
     bool lightIsOn = root["isOn"];
-    Serial.println(lightIsOn ? "Light on" : "Light off");
     btn.setActive(lightIsOn);
-    
+
+    http.end();
     return true;
-  }  
+  } 
+
+  Serial.println("Wifi not connected");
+  return false;
 }
 
 void callHTTP(const char* szUrl) {
     // wait for WiFi connection
     if((WiFiMulti.run() == WL_CONNECTED)) {
+
+        Serial.print("Calling: ");
+        Serial.println(szUrl);
 
         HTTPClient http;
 
@@ -110,8 +142,31 @@ void callHTTP(const char* szUrl) {
     }
 }
 
-void loop() {
+void loop() {  
 
-    nex.poll();
+  // if time elapsed, confirm light status
+  // (in case the light has been turned on externally)
+  --g_updateWait;
+  if (g_updateWait <= 0) {
+    if (refreshLightData()) {
+      g_prevButtonState = btn.isActive();
+    }
+
+    g_updateWait = REFRESH_LIGHT_COUNT;
+  }
+
+  // check button state
+  bool btnActive = btn.isActive();
+  if (btnActive != g_prevButtonState) {
+    if (btnActive) {
+      callHTTP(LIGHT_API "/on");
+    } else {      
+      callHTTP(LIGHT_API "/off");
+    }
+
+    g_prevButtonState = btnActive;
+  }
+  
+  delay(DELAY_MILLIS);
 }
 
